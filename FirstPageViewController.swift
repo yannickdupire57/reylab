ac5d2650bad74bf211ae3d18d25a14467857a586
yannickdupire57 <yannick@dupire.info>
2017-09-22 09:46:19 +0200

    conversion swift 4

---
+++ FirstPageViewController.swift
@@ -0,0 +1,40 @@
+//
+//  FirstPageViewController.swift
+//  reylab
+//
+//  Created by yannick dupire on 18/09/2017.
+//  Copyright © 2017 Yannick Dupire. All rights reserved.
+//
+
+import UIKit
+
+
+class FirstPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    func firstButton(){
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
